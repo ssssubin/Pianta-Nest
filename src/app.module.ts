6e1ee9e7d3895cb154f9d37fdb12b1a51d04f350
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-01 00:18:55 +0900

    Docs: 환경 변수 설정, 회원 테이블 생성하는 코드 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, OnModuleInit } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AccountModule } from "./account/account.module";
+import { MySqlModule } from "./my-sql/my-sql.module";
+import { ConfigModule } from "@nestjs/config";
+import { MySqlService } from "./my-sql/my-sql.service";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+   imports: [
+      AccountModule,
+      MySqlModule,
+      // isGlobal: 전역 모듈로 선언
+      ConfigModule.forRoot({ envFilePath: ".env", isGlobal: true }),
+   ],
+   controllers: [AppController],
+   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+   constructor(private readonly mysqlService: MySqlService) {}
+   async onModuleInit() {
+      // 서버 시작 시 테이블 생성
+      await this.mysqlService.createUserTable();
+   }
+}
