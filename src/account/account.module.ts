4954d41c40708b8d5f57d5042ff5443b3b76cf72
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-04 17:20:02 +0900

    Docs: JwtModule 전역으로 등록

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -2,8 +2,17 @@ import { Module } from "@nestjs/common";
 import { AccountService } from "./account.service";
 import { AccountController } from "./account.controller";
 import { MySqlService } from "src/my-sql/my-sql.service";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
+   imports: [
+      JwtModule.registerAsync({
+         useFactory: async () => ({
+            secret: process.env.USER_JWT_SECRET_KEY,
+            signOptions: { expiresIn: "1h" },
+         }),
+      }),
+   ],
    providers: [AccountService, MySqlService], // accountService와 mysqlService 등록
    controllers: [AccountController],
 })
