0d776e36900267664b8cdff0c517b911a1fa5079
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-08 12:06:06 +0900

    Comment: 주석 추가 Feat: 비회원 로그인 및 로그아웃 기능 구현

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,23 +1,27 @@
-import { Body, Controller, Post, Res } from "@nestjs/common";
+import { Body, Controller, Post, Req, Res } from "@nestjs/common";
 import { createUserDto } from "src/account/dto/create-user.dto";
 import { AccountService } from "./account.service";
-import { Response } from "express";
+import { Request, Response } from "express";
 import { signInUserDto } from "src/account/dto/sign-in-user.dto";
+import { signInGuestDto } from "./dto/sign-in-guest.dto";
 
 @Controller()
 export class AccountController {
    constructor(private readonly accountService: AccountService) {}
 
+   // 회원가입
    @Post("sign-up")
    async signUp(@Body() userData: createUserDto) {
       return await this.accountService.createUser(userData);
    }
 
+   // 이메일 중복 체크
    @Post("sign-up/check-email")
    async checkEmail(@Body() userData: signInUserDto) {
       return await this.accountService.checkEmail(userData.email);
    }
 
+   // 회원 로그인
    @Post("sign-in")
    async signIn(@Res({ passthrough: true }) res: Response, @Body() userData: signInUserDto) {
       const { jwtToken, isAdmin } = await this.accountService.signIn(userData);
@@ -27,5 +31,20 @@ export class AccountController {
       }
       res.cookie("userCookies", jwtToken, { httpOnly: true });
       return { err: null, data: { isAdmin: false, message: "로그인에 성공하셨습니다. 환영합니다." } };
+   }
+
+   // 비회원 로그인
+   @Post("guest/sign-in")
+   async guestSignIn(@Res({ passthrough: true }) res: Response, @Body() guestData: signInGuestDto) {
+      const { jwtToken } = await this.accountService.guestSignIn(guestData);
+      res.cookie("guestCookies", jwtToken, { httpOnly: true });
+      return { err: null, message: "인증 성공하였습니다." };
+   }
+
+   // 로그아웃
+   @Post("sign-out")
+   async signOut(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
+      await this.accountService.signOut(req, res);
+      return { err: null, data: "성공적으로 로그아웃 되었습니다." };
    }
 }
