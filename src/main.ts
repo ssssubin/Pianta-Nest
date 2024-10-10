1df89dbec853616407990e2765855a38e932874a
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-10 21:32:43 +0900

    Docs: 모든 경로에 "/api" 붙이기 위한 코드 추가

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
    const app = await NestFactory.create(AppModule);
+   app.setGlobalPrefix("/api"); // 모든 경로에 /api 붙이기 위함
    app.useGlobalPipes(
       new ValidationPipe({
          whitelist: true,
