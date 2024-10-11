1bfd67b33e94daf9f190c6ccd1b0b5076430c829
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-11 11:51:41 +0900

    Feat: 마이페이지 수정 API 구현

--- src/my-page/my-page.controller.ts
+++ src/my-page/my-page.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get, Req, Res } from "@nestjs/common";
+import { Body, Controller, Get, Put, Req, Res } from "@nestjs/common";
 import { Request, Response } from "express";
 import { MyPageService } from "./my-page.service";
+import { signInUserDto } from "src/account/dto/sign-in-user.dto";
 
 @Controller("my-page")
 export class MyPageController {
    constructor(private mypageService: MyPageService) {}
-   @Get()
+   @Get() // 마이페이지 조회
    async getMyPage(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
       return await this.mypageService.getMyPage(req, res);
+   }
+
+   @Put() // 마이페이지 수정
+   async updateMyPage(@Res({ passthrough: true }) res: Response, @Body() updateData: signInUserDto) {
+      const { newJwtToken, data } = await this.mypageService.updateMyPage(res, updateData);
+      res.cookie("userCookies", newJwtToken, { httpOnly: true });
+      return { err: null, data };
    }
 }
--- src/my-page/my-page.service.ts
+++ src/my-page/my-page.service.ts
@@ -1,11 +1,17 @@
-import { ForbiddenException, Injectable, NotFoundException, Req, Res } from "@nestjs/common";
+import { ForbiddenException, Injectable, NotFoundException, PreconditionFailedException } from "@nestjs/common";
 import { Request, Response } from "express";
 import { MySqlService } from "src/my-sql/my-sql.service";
-
+import * as bcrypt from "bcrypt";
+import { JwtService } from "@nestjs/jwt";
+import { signInUserDto } from "src/account/dto/sign-in-user.dto";
 @Injectable()
 export class MyPageService {
-   constructor(private mysqlService: MySqlService) {}
-   async getMyPage(@Req() req: Request, @Res() res: Response) {
+   constructor(
+      private mysqlService: MySqlService,
+      private jwtService: JwtService,
+   ) {}
+   // 마이페이지 조회
+   async getMyPage(req: Request, res: Response) {
       try {
          const { adminCookies } = req.cookies;
 
@@ -14,9 +20,7 @@ export class MyPageService {
             throw new ForbiddenException("접근 권한이 없습니다.");
          }
 
-         const sql = `SELECT * FROM users WHERE email = ?`;
-         const params = [res.locals.user.email];
-         const foundUser = await this.mysqlService.query(sql, params);
+         const foundUser = await this.mysqlService.findUser(res.locals.user.email);
 
          if (foundUser[0] === undefined) {
             throw new NotFoundException("해당 유저를 찾을 수 없습니다.");
@@ -29,6 +33,58 @@ export class MyPageService {
                email: foundUser[0].email,
                address: foundUser[0].address,
                phoneNumber: foundUser[0].phone_number,
+            },
+         };
+      } catch (e) {
+         throw e;
+      }
+   }
+
+   // 마이페이지 수정
+   async updateMyPage(res: Response, updateData: signInUserDto) {
+      try {
+         const foundUser = await this.mysqlService.findUser(res.locals.user.email);
+         // 유저가 존재하지 않는 경우
+         if (foundUser[0] === undefined) {
+            throw new NotFoundException("해당 유저를 찾을 수 없습니다.");
+         }
+
+         // 비밀번호 재확인 안 했을 경우
+         if (foundUser[0].update_lock === 1) {
+            throw new PreconditionFailedException("비밀번호를 재확인해야 합니다.");
+         }
+
+         const hashPassword = await bcrypt.hash(updateData.password, 10);
+         const address = updateData.postNumber + " " + updateData.address + " " + updateData.detailAddress;
+         const sql = `UPDATE users 
+         SET email = ?, name = ?, password =?, address = ?, phone_number = ?, update_lock = ?
+         WHERE email = ?`;
+         const params = [
+            updateData.email,
+            updateData.name,
+            hashPassword,
+            address,
+            updateData.phoneNumber,
+            true,
+            foundUser[0].email,
+         ];
+         await this.mysqlService.query(sql, params);
+
+         const newJwtToken = await this.jwtService.signAsync(
+            {
+               email: updateData.email,
+               phone_number: updateData.phoneNumber,
+            },
+            { secret: process.env.USER_JWT_SECRET_KEY },
+         );
+
+         return {
+            newJwtToken,
+            data: {
+               email: updateData.email,
+               name: updateData.name,
+               address,
+               phoneNumber: updateData.phoneNumber,
             },
          };
       } catch (e) {
