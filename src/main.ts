b372bf01e734e492a2cc20c61c2ce83d0a994f78
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-14 17:15:13 +0900

    Docs: swagger 설정

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import * as cookieParser from "cookie-parser";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
    const app = await NestFactory.create(AppModule);
    app.setGlobalPrefix("/api"); // 모든 경로에 /api 붙이기 위함
+   // swagger 설정
+   const config = new DocumentBuilder()
+      .setTitle("Pianta API 문서")
+      .setDescription("식물 판매 사이트 API 문서")
+      .setVersion("1.0.0")
+      .addCookieAuth("userCookies")
+      .build();
+   const document = SwaggerModule.createDocument(app, config);
+   SwaggerModule.setup("api-docs", app, document);
+
    app.useGlobalPipes(
       new ValidationPipe({
          whitelist: true,
