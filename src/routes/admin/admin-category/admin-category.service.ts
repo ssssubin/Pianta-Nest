c0599e4d0f66a6f8fcf4f1a6bbf7bdba95bfeb6d
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-15 10:47:09 +0900

    Docs: 코드 수정

--- src/routes/admin/admin-category/admin-category.service.ts
+++ src/routes/admin/admin-category/admin-category.service.ts
@@ -6,17 +6,18 @@ import { MySqlService } from "src/data/my-sql/my-sql.service";
 export class AdminCategoryService {
    constructor(private mysqlService: MySqlService) {}
    async createCategory(category: createCategoryDto) {
-      const sql = `SELECT count(*) FROM categories WHERE ?= ?`;
-      const numberParams = ["number", category.number];
+      const numberSql = `SELECT count(*) as count FROM categories WHERE number= ?`;
+      const numberParams = [category.number];
       // 대분류 카테고리 번호가 db에 존재하는 경우
-      const foundCategory = await this.mysqlService.query(sql, numberParams);
+      const foundCategory = await this.mysqlService.query(numberSql, numberParams);
       if (foundCategory[0].count !== 0) {
          throw new BadRequestException("이미 존재하는 대분류 카테고리 번호입니다.");
       }
 
+      const nameSql = `SELECT count(*) as count FROM categories WHERE name= ?`;
+      const nameParams = [category.name];
+      const foundCategoryName = await this.mysqlService.query(nameSql, nameParams);
       // 대분류 카테고리 이름이 db에 존재하는 경우
-      const nameParams = ["name", category.name];
-      const foundCategoryName = await this.mysqlService.query(sql, nameParams);
       if (foundCategoryName[0].count !== 0) {
          throw new BadRequestException("이미 존재하는 대분류 카테고리 이름입니다.");
       }
@@ -24,7 +25,7 @@ export class AdminCategoryService {
       // 새로운 카테고리 생성
       const createSql = `INSERT INTO categories VALUES(?, ?)`;
       const createParams = [category.number, category.name];
-      const createdCategory = await this.mysqlService.query(createSql, createParams);
-      return { err: null, data: { categoryNumber: createdCategory[0].number, categoryName: createdCategory[0].name } };
+      await this.mysqlService.query(createSql, createParams);
+      return { err: null, data: { categoryNumber: category.number, categoryName: category.name } };
    }
 }
