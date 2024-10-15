e1d6768f2c125e04b26dbe3647839684dd5273c1
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-15 10:47:58 +0900

    Docs: 유효성 검사 코드 수정

--- src/routes/admin/dto/create-category.dto.ts
+++ src/routes/admin/dto/create-category.dto.ts
@@ -1,10 +1,14 @@
-import { IsNumber, IsString, Length } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsInt, IsString, Max, Min } from "class-validator";
 
 export class createCategoryDto {
-   @Length(1, 1, { message: "대분류 카테고리는 1자리입니다." })
-   @IsNumber({}, { message: "대분류 카테고리는 숫자입니다." })
+   @ApiProperty({ description: "대분류 카테고리 번호" })
+   @IsInt({ message: "대분류 카테고리 번호는 정수입니다." })
+   @Min(1, { message: "대분류 카테고리 번호는 1자리 숫자여야 합니다." })
+   @Max(9, { message: "대분류 카테고리 번호는 1자리 숫자여야 합니다." })
    readonly number: number;
 
+   @ApiProperty({ description: "대분류 카테고리 이름" })
    @IsString({ message: "대분류 카테고리 이름은 문자열이어야 합니다." })
    readonly name: string;
 }
