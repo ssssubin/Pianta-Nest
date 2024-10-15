75b5819fa8e54a4be49ac8bc4c32cd1094c5112b
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-15 10:46:04 +0900

    Docs: 코드 수정 - console 삭제 - 예외처리 코드 추가

--- src/middleware/is-authenticated-admin.middleware.ts
+++ src/middleware/is-authenticated-admin.middleware.ts
@@ -1,4 +1,4 @@
-import { ForbiddenException, Injectable, NestMiddleware } from "@nestjs/common";
+import { ForbiddenException, Injectable, NestMiddleware, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { NextFunction, Request, Response } from "express";
 
@@ -11,8 +11,21 @@ export class IsAuthenticatedAdminMiddleware implements NestMiddleware {
          throw new ForbiddenException("관리자가 아닙니다.");
       }
       let token = adminCookies;
-      const jwt = await this.jwtService.verify(token, { secret: process.env.USER_JWT_SECRET_KEY });
-      console.log(jwt);
+      const jwt = await this.jwtService.verifyAsync(token, { secret: process.env.USER_JWT_SECRET_KEY }).catch((e) => {
+         if (e) {
+            if (e.name === "TokenExpiredError") {
+               res.clearCookie("adminCookies");
+               throw new UnauthorizedException("토큰이 만료되었습니다. 다시 로그인해주세요.");
+            }
+
+            if (e.name === "JsonWebTokenError") {
+               res.clearCookie("adminCookies");
+               throw new UnauthorizedException("유효하지 않거나 손상된 토큰입니다. 다시 로그인해주세요.");
+            }
+         }
+      });
+
+      res.locals.admin = jwt;
       next();
    }
 }
