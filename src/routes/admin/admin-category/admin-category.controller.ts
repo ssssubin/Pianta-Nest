9751e05630ba29f5d3fe1a7827ecd133c477bd9d
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-16 10:14:57 +0900

    Feat: 대분류 카테고리 수정 및 삭제 API 구현

--- src/routes/admin/admin-category/admin-category.controller.ts
+++ src/routes/admin/admin-category/admin-category.controller.ts
@@ -1,16 +1,22 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Param, Post, Put, Res } from "@nestjs/common";
 import { createCategoryDto } from "../dto/create-category.dto";
 import { AdminCategoryService } from "./admin-category.service";
 import {
    ApiBadRequestResponse,
+   ApiBody,
    ApiCookieAuth,
    ApiCreatedResponse,
    ApiForbiddenResponse,
    ApiInternalServerErrorResponse,
+   ApiNoContentResponse,
+   ApiNotFoundResponse,
+   ApiOkResponse,
    ApiOperation,
    ApiTags,
    ApiUnauthorizedResponse,
 } from "@nestjs/swagger";
+import { updateCategoryDto } from "../dto/update-category.dto";
+import { Response } from "express";
 @ApiCookieAuth("adminCookies")
 @ApiTags("카테고리 API")
 @Controller("admin/category")
@@ -26,7 +32,7 @@ export class AdminCategoryController {
    @ApiBadRequestResponse({
       description: "Bad Request",
       example: {
-         err: "대분류 카테고리는 1자리입니다. | 대분류 카테고리는 숫자입니다. | 대분류 카테고리 이름은 문자열이어야 합니다. | 이미 존재하는 대분류 카테고리 번호입니다. | 이미 존재하는 대분류 카테고리 이름입니다.",
+         err: "대분류 카테고리는 1자리입니다. | 대분류 카테고리는 숫자입니다. | 대분류 카테고리 이름은 문자열이어야 합니다. | 대분류 카테고리명은 빈 값이 아니어야 합니다. | 이미 존재하는 대분류 카테고리 번호입니다. | 이미 존재하는 대분류 카테고리 이름입니다.",
          data: null,
       },
    })
@@ -41,5 +47,57 @@ export class AdminCategoryController {
    @ApiInternalServerErrorResponse({ description: "Internal Server Error" })
    async createCategory(@Body() category: createCategoryDto) {
       return await this.adminCategoryService.createCategory(category);
+   }
+
+   // 대분류 카테고리 수정
+   @Put(":categoryNumber")
+   @ApiOperation({ summary: "대분류 카테고리 수정 API" })
+   @ApiBody({ schema: { example: { name: "string" } } })
+   @ApiOkResponse({
+      description: "대분류 카테고리 수정",
+      example: { err: null, data: { categoryNumber: 1, categoryName: "꽃" } },
+   })
+   @ApiBadRequestResponse({
+      description: "Bad Request",
+      example: {
+         err: "대분류 카테고리 번호는 정수입니다. | 대분류 카테고리 번호는 1자리 숫자여야 합니다. | 대분류 카테고리명은 문자열이어야 합니다. | 대분류 카테고리명은 빈 값이 아니어야 합니다. | 이미 존재하는 대분류 카테고리 이름입니다.",
+         data: null,
+      },
+   })
+   @ApiUnauthorizedResponse({
+      description: "UnAuthorized",
+      example: {
+         err: "토큰이 만료되었습니다. 다시 로그인해주세요. | 유효하지 않거나 손상된 토큰입니다. 다시 로그인해주세요.",
+         data: null,
+      },
+   })
+   @ApiForbiddenResponse({ description: "Forbidden", example: { err: "관리자가 아닙니다.", data: null } })
+   @ApiNotFoundResponse({
+      description: "Not Found",
+      example: { err: "존재하지 않는 대분류 카테고리입니다.", data: null },
+   })
+   @ApiInternalServerErrorResponse({ description: "Internal Server Error" })
+   async updateCategory(@Param("categoryNumber") category: number, @Body() name: updateCategoryDto) {
+      return await this.adminCategoryService.updateCategory(category, name);
+   }
+
+   // 대분류 카테고리 삭제
+   @Delete(":categoryNumber")
+   @ApiNoContentResponse({ description: "대분류 카테고리 삭제" })
+   @ApiUnauthorizedResponse({
+      description: "UnAuthorized",
+      example: {
+         err: "토큰이 만료되었습니다. 다시 로그인해주세요. | 유효하지 않거나 손상된 토큰입니다. 다시 로그인해주세요.",
+         data: null,
+      },
+   })
+   @ApiForbiddenResponse({ description: "Forbidden", example: { err: "관리자가 아닙니다.", data: null } })
+   @ApiNotFoundResponse({
+      description: "Not Found",
+      example: { err: "존재하지 않는 대분류 카테고리입니다.", data: null },
+   })
+   @ApiInternalServerErrorResponse({ description: "Internal Server Error" })
+   async deleteCategory(@Res({ passthrough: true }) res: Response, @Param("categoryNumber") category: number) {
+      return await this.adminCategoryService.deleteCategory(res, category);
    }
 }
--- src/routes/admin/admin-category/admin-category.service.ts
+++ src/routes/admin/admin-category/admin-category.service.ts
@@ -1,31 +1,80 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { createCategoryDto } from "../dto/create-category.dto";
 import { MySqlService } from "src/data/my-sql/my-sql.service";
+import { updateCategoryDto } from "../dto/update-category.dto";
+import { Response } from "express";
 
 @Injectable()
 export class AdminCategoryService {
    constructor(private mysqlService: MySqlService) {}
    async createCategory(category: createCategoryDto) {
-      const numberSql = `SELECT count(*) as count FROM categories WHERE number= ?`;
-      const numberParams = [category.number];
-      // 대분류 카테고리 번호가 db에 존재하는 경우
-      const foundCategory = await this.mysqlService.query(numberSql, numberParams);
-      if (foundCategory[0].count !== 0) {
-         throw new BadRequestException("이미 존재하는 대분류 카테고리 번호입니다.");
-      }
+      try {
+         const numberSql = `SELECT count(*) as count FROM categories WHERE number= ?`;
+         const numberParams = [category.number];
+         // 대분류 카테고리 번호가 db에 존재하는 경우
+         const foundCategory = await this.mysqlService.query(numberSql, numberParams);
+         if (foundCategory[0].count !== 0) {
+            throw new BadRequestException("이미 존재하는 대분류 카테고리 번호입니다.");
+         }
 
-      const nameSql = `SELECT count(*) as count FROM categories WHERE name= ?`;
-      const nameParams = [category.name];
-      const foundCategoryName = await this.mysqlService.query(nameSql, nameParams);
-      // 대분류 카테고리 이름이 db에 존재하는 경우
-      if (foundCategoryName[0].count !== 0) {
-         throw new BadRequestException("이미 존재하는 대분류 카테고리 이름입니다.");
-      }
+         const nameSql = `SELECT count(*) as count FROM categories WHERE name= ?`;
+         const nameParams = [category.name];
+         const foundCategoryName = await this.mysqlService.query(nameSql, nameParams);
+         // 대분류 카테고리 이름이 db에 존재하는 경우
+         if (foundCategoryName[0].count !== 0) {
+            throw new BadRequestException("이미 존재하는 대분류 카테고리명입니다");
+         }
 
-      // 새로운 카테고리 생성
-      const createSql = `INSERT INTO categories VALUES(?, ?)`;
-      const createParams = [category.number, category.name];
-      await this.mysqlService.query(createSql, createParams);
-      return { err: null, data: { categoryNumber: category.number, categoryName: category.name } };
+         // 새로운 카테고리 생성
+         const createSql = `INSERT INTO categories VALUES(?, ?)`;
+         const createParams = [category.number, category.name];
+         await this.mysqlService.query(createSql, createParams);
+         return { err: null, data: { categoryNumber: category.number, categoryName: category.name } };
+      } catch (e) {
+         throw e;
+      }
+   }
+
+   async updateCategory(category: number, categoryName: updateCategoryDto) {
+      try {
+         const numberSql = `SELECT count(*) as count FROM categories WHERE number = ?`;
+         const numberParams = [category];
+         const foundCategoryNumber = await this.mysqlService.query(numberSql, numberParams);
+         if (foundCategoryNumber[0].count === 0) {
+            throw new NotFoundException("존재하지 않는 대분류 카테고리입니다.");
+         }
+
+         const nameSql = `SELECT count(*) as count FROM categories WHERE name = ?`;
+         const nameParams = [categoryName.name];
+         const foundCategoryName = await this.mysqlService.query(nameSql, nameParams);
+         if (foundCategoryName[0].count !== 0) {
+            throw new BadRequestException("이미 존재하는 대분류 카테고리명입니다.");
+         }
+
+         const updateSql = `UPDATE categories SET name = ? WHERE number= ?`;
+         const updateParams = [categoryName.name, category];
+         await this.mysqlService.query(updateSql, updateParams);
+         return { err: null, data: { categoryNumber: category, categoryName: categoryName.name } };
+      } catch (e) {
+         throw e;
+      }
+   }
+
+   async deleteCategory(res: Response, category: number) {
+      try {
+         const sql = `SELECT COUNT(*) as count FROM categories WHERE number = ?`;
+         const params = [category];
+         const foundCategory = await this.mysqlService.query(sql, params);
+         if (foundCategory[0].count === 0) {
+            throw new NotFoundException("존재하지 않는 대분류 카테고리입니다.");
+         }
+
+         const deleteSql = `DELETE FROM categories WHERE number = ?`;
+         await this.mysqlService.query(deleteSql, params);
+         res.status(204);
+         return;
+      } catch (e) {
+         throw e;
+      }
    }
 }
