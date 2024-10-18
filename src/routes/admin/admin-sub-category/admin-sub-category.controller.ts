5ad60ea6e28d3276d7727978e6c99982c3844301
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-18 09:28:51 +0900

    Feat: 소분류 카테고리 수정 API  구현

--- src/routes/admin/admin-sub-category/admin-sub-category.controller.ts
+++ src/routes/admin/admin-sub-category/admin-sub-category.controller.ts
@@ -1,17 +1,20 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Param, Post, Put } from "@nestjs/common";
 import {
    ApiBadRequestResponse,
+   ApiBody,
    ApiCookieAuth,
    ApiCreatedResponse,
    ApiForbiddenResponse,
    ApiInternalServerErrorResponse,
    ApiNotFoundResponse,
+   ApiOkResponse,
    ApiOperation,
    ApiTags,
    ApiUnauthorizedResponse,
 } from "@nestjs/swagger";
 import { createSubCategoryDto } from "../dto/create-sub-category.dto";
 import { AdminSubCategoryService } from "./admin-sub-category.service";
+import { updateSubCategoryDto } from "../dto/update-sub-category.dto";
 
 @ApiTags("카테고리 API")
 @ApiCookieAuth("adminCookies")
@@ -48,5 +51,37 @@ export class AdminSubCategoryController {
    @ApiInternalServerErrorResponse({ description: "Internal Server Error" })
    async createSubCategory(@Body() subCategoryData: createSubCategoryDto) {
       return this.subCategoryService.createSubCategory(subCategoryData);
+   }
+
+   // 소분류 카테고리 수정
+   @Put(":subCategoryNumber")
+   @ApiOperation({ summary: "소분류 카테고리 수정 API" })
+   @ApiBody({ schema: { example: { name: "string" } } })
+   @ApiOkResponse({ description: "소분류 카테고리 수정", example: { err: null, data: { subCategoryName: "장미" } } })
+   @ApiBadRequestResponse({
+      description: "Bad Request",
+      example: {
+         err: "소분류 카테고리 번호는 정수여야 합니다. | 소분류 카테고리 번호는 3자리 숫자여야 합니다.| 소분류 카테고리명은 문자열이어야 합니다. | 소분류 카테고리명은 빈 값이 아니어야 합니다. | 이미 존재하는 소분류 카테고리명입니다.",
+         date: null,
+      },
+   })
+   @ApiUnauthorizedResponse({
+      description: "UnAuthorized",
+      example: {
+         err: "토큰이 만료되었습니다. 다시 로그인해주세요. | 유효하지 않거나 손상된 토큰입니다. 다시 로그인해주세요.",
+         data: null,
+      },
+   })
+   @ApiForbiddenResponse({ description: "Forbidden", example: { err: "관리자가 아닙니다.", data: null } })
+   @ApiNotFoundResponse({
+      description: "Not Found",
+      example: { err: "존재하지 않는 소분류 카테고리입니다.", data: null },
+   })
+   @ApiInternalServerErrorResponse({ description: "Internal Server Error" })
+   async updateSubCategory(
+      @Param("subCategoryNumber") subCategory: number,
+      @Body() subCategoryName: updateSubCategoryDto,
+   ) {
+      return this.subCategoryService.updateSubCategory(subCategory, subCategoryName);
    }
 }
--- src/routes/admin/admin-sub-category/admin-sub-category.service.ts
+++ src/routes/admin/admin-sub-category/admin-sub-category.service.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { MySqlService } from "src/data/my-sql/my-sql.service";
 import { createSubCategoryDto } from "../dto/create-sub-category.dto";
+import { updateSubCategoryDto } from "../dto/update-sub-category.dto";
 
 @Injectable()
 export class AdminSubCategoryService {
@@ -42,6 +43,32 @@ export class AdminSubCategoryService {
                mainCategoryNumber: subCategoryData.mainCategoryNumber,
             },
          };
+      } catch (e) {
+         throw e;
+      }
+   }
+
+   async updateSubCategory(subCategory: number, subCategoryName: updateSubCategoryDto) {
+      try {
+         const subCategorySql = `SELECT COUNT(*) as count FROM subcategories WHERE number = ?`;
+         const subCategoryParams = [subCategory];
+         const foundSubCategory = await this.mySqlService.query(subCategorySql, subCategoryParams);
+         // 소분류 카테고리 db 존재 여부
+         if (foundSubCategory[0].count === 0) {
+            throw new NotFoundException("존재하지 않는 소분류 카테고리입니다.");
+         }
+
+         const subCategoryNameSql = `SELECT COUNT(*) as count FROM subcategories WHERE name = ?`;
+         const subCategoryNameParmas = [subCategoryName.name];
+         const foundSubCategoryName = await this.mySqlService.query(subCategoryNameSql, subCategoryNameParmas);
+         if (foundSubCategoryName[0].count !== 0) {
+            throw new BadRequestException("이미 존재하는 소분류 카테고리명입니다.");
+         }
+
+         const updateSql = `UPDATE subcategories SET name = ? WHERE number = ?`;
+         const updateParams = [subCategoryName.name, subCategory];
+         await this.mySqlService.query(updateSql, updateParams);
+         return { err: null, data: { subCategoryName: subCategoryName.name } };
       } catch (e) {
          throw e;
       }
