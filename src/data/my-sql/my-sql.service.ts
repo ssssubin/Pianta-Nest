a433e1f7e68f8ddcc3ad53b3905a18c882cbf08e
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-20 17:08:36 +0900

    Docs: 외래키 조건 추가

--- src/data/my-sql/my-sql.service.ts
+++ src/data/my-sql/my-sql.service.ts
@@ -68,15 +68,15 @@ export class MySqlService {
    // 상품 테이블 생성
    async createProducTable() {
       const sql = `CREATE TABLE IF NOT EXISTS products(
-         number BIGINT(12) PRIMARY KEY,
+         number BIGINT(20) PRIMARY KEY,
          name VARCHAR(255) NOT NULL,
          price INT(10) NOT NULL,
          information VARCHAR(1000),
          origin VARCHAR(50),
          category_number INT(1) NOT NULL,
          sub_category_number INT(3) NOT NULL,
-         FOREIGN KEY (category_number) REFERENCES categories (number),
-         FOREIGN KEY (sub_category_number) REFERENCES subcategories (number)
+         FOREIGN KEY (category_number) REFERENCES categories (number) ON UPDATE CASCADE ON DELETE CASCADE,
+         FOREIGN KEY (sub_category_number) REFERENCES subcategories (number) ON UPDATE CASCADE ON DELETE CASCADE
       )`;
       await this.pool.execute(sql);
    }
