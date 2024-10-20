028624216ea327af39d95348e06c2bfff4eb7b54
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-20 17:36:07 +0900

    Feat: 상품 수정 API 구현

--- src/routes/admin/admin-product/admin-product.controller.ts
+++ src/routes/admin/admin-product/admin-product.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
 import {
    ApiBadRequestResponse,
    ApiCookieAuth,
    ApiCreatedResponse,
    ApiForbiddenResponse,
    ApiInternalServerErrorResponse,
+   ApiNotFoundResponse,
    ApiOkResponse,
    ApiOperation,
    ApiTags,
@@ -90,5 +91,44 @@ export class AdminProductController {
    @ApiInternalServerErrorResponse({ description: "Internal Server Error" })
    async createProduct(@Body() productData: createProductDto) {
       return await this.productService.createProduct(productData);
+   }
+
+   // 상품 수정
+   @ApiOperation({ summary: "상품 수정 API" })
+   @Put(":productNumber")
+   @ApiOkResponse({
+      description: "상품 수정",
+      example: {
+         err: null,
+         data: {
+            number: "17294115786087126",
+            name: "test",
+            price: 100000,
+            information: "test",
+            origin: "test",
+            categoryNumber: 1,
+            subCategoryNumber: 101,
+         },
+      },
+   })
+   @ApiBadRequestResponse({
+      description: "Bad Request",
+      example: {
+         err: "상품명은 문자열이어야 합니다. | 상품명은 빈 값이 아니어야 합니다. | 가격은 정수여야 합니다. | 가격은 양수여야 합니다. | 상품 정보는 문자열이어야 합니다. | 원산지는 문자열이어야 합니다. | 대분류 카테고리명은 문자열이어야 합니다. | 대분류 카테고리명은 빈 값이 아니어야 합니다. | 소분류 카테고리명은 문자열이어야 합니다. | 소분류 카테고리명은 빈 값이 아니어야 합니다. | 존재하지 않는 대분류 카테고리입니다. | 존재하지 않는 소분류 카테고리입니다.",
+         data: null,
+      },
+   })
+   @ApiUnauthorizedResponse({
+      description: "UnAuthorized",
+      example: {
+         err: "토큰이 만료되었습니다. 다시 로그인해주세요. | 유효하지 않거나 손상된 토큰입니다. 다시 로그인해주세요.",
+         data: null,
+      },
+   })
+   @ApiForbiddenResponse({ description: "Forbidden", example: { err: "관리자가 아닙니다.", data: null } })
+   @ApiNotFoundResponse({ description: "Not Found", example: { err: "존재하지 않는 상품입니다.", data: null } })
+   @ApiInternalServerErrorResponse({ description: "Internal Server Error" })
+   async updateProduct(@Param("productNumber") product: number, @Body() updateData: createProductDto) {
+      return await this.productService.updateProduct(product, updateData);
    }
 }
