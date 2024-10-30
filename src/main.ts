307a7cd4f25af9774815585ab7448e49ca603a6f
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-30 10:26:12 +0900

    🔧 http 예외처리필터 전역 필터로 설정

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import * as cookieParser from "cookie-parser";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { httpExceptionFilter } from "./filter/http-exception.filter";
 
 async function bootstrap() {
    const app = await NestFactory.create(AppModule);
    app.setGlobalPrefix("/api"); // 모든 경로에 /api 붙이기 위함
+   app.useGlobalFilters(new httpExceptionFilter());
    // swagger 설정
    const config = new DocumentBuilder()
       .setTitle("Pianta API 문서")
