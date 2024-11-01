0173b7246fedf9e666871358f32a8bd19b73b4d2
[IoT3] 조수빈 <cdw3521@naver.com>
2024-11-01 10:56:04 +0900

    ✨ 소분류 카테고리별 상품 조회 API 구현

--- src/routes/user/product/product.service.ts
+++ src/routes/user/product/product.service.ts
@@ -56,4 +56,38 @@ export class ProductService {
          throw e;
       }
    }
+
+   async getProductBySubCategory(category: number, subCategory: number) {
+      try {
+         const categorySql = `SELECT COUNT(*) as count FROM categories WHERE number = ?`;
+         const categoryParams = [category];
+         const foundCategory = await this.mysqlService.query(categorySql, categoryParams);
+         // 대분류 카테고리가 존재하지 않을 경우
+         if (foundCategory[0].count === 0) {
+            throw new NotFoundException("존재하지 않는 대분류 카테고리입니다.");
+         }
+
+         const subCategorySql = `SELECT name, main_category_number FROM subcategories WHERE number = ?`;
+         const subCategoryParams = [subCategory];
+         const foundSubCategory = await this.mysqlService.query(subCategorySql, subCategoryParams);
+         // 소분류 카테고리가 존재하지 않을 경우
+         if (foundSubCategory[0] === undefined) {
+            throw new NotFoundException("존재하지 않는 소분류 카테고리입니다.");
+         }
+
+         // 대분류 카테고리에 소분류 카테고리가 존재하지 않는 경우
+         if (foundSubCategory[0].main_category_number !== category) {
+            throw new NotFoundException("대분류 카테고리에 존재하지 않는 소분류 카테고리입니다.");
+         }
+
+         const productSql = `SELECT * FROM products WHERE category_number= ? and sub_category_number = ?`;
+         const foundProduct = await this.mysqlService.query(productSql, [category, subCategory]);
+         const products = Object.values(foundProduct).map((product) => {
+            return { name: product.name, price: product.price };
+         });
+         return { err: null, data: { subCategoryName: foundSubCategory[0].name, products } };
+      } catch (e) {
+         throw e;
+      }
+   }
 }
