d51176c94959f2e50f725d335e48386b84ea357c
[IoT3] 조수빈 <cdw3521@naver.com>
2024-11-04 11:53:17 +0900

    ✨ 주문 controller 및 provider 추가

--- src/routes/user/user.module.ts
+++ src/routes/user/user.module.ts
@@ -5,12 +5,14 @@ import { JwtModule } from "@nestjs/jwt";
 import { MySqlService } from "src/data/my-sql/my-sql.service";
 import { MyPageController } from "./my-page/my-page.controller";
 import { MyPageService } from "./my-page/my-page.service";
+import { OrderController } from "./order/order.controller";
+import { OrderService } from "./order/order.service";
 import { ProductController } from "./product/product.controller";
 import { ProductService } from "./product/product.service";
 
 @Module({
    imports: [JwtModule],
-   controllers: [AccountController, MyPageController, ProductController],
-   providers: [AccountService, MyPageService, MySqlService, ProductService],
+   controllers: [AccountController, MyPageController, OrderController, ProductController],
+   providers: [AccountService, MyPageService, MySqlService, OrderService, ProductService],
 })
 export class UserModule {}
