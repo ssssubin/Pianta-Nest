cead883d2f0decadb7e73ac9aa481f2adac21865
subin <cdw3521@naver.com>
2024-11-11 17:07:50 +0900

    🚚 productDto -> create-order.dto.ts 파일로 이동

--- src/routes/user/dto/create-order.dto.ts
+++ src/routes/user/dto/create-order.dto.ts
@@ -1,11 +1,12 @@
-import { ApiProperty, OmitType } from "@nestjs/swagger";
-import { productDto } from "./product.dto";
+import { ApiProperty, OmitType, PickType } from "@nestjs/swagger";
 import { createUserDto } from "./create-user.dto";
 import { Type } from "class-transformer";
 import { ValidateNested } from "class-validator";
+import { createProductDto } from "src/routes/admin/dto/create-product.dto";
 
 export class userOrderDto extends OmitType(createUserDto, ["password", "confirmPassword", "agreement"] as const) {}
 export class guestOrderDto extends OmitType(createUserDto, ["agreement"] as const) {}
+export class productDto extends PickType(createProductDto, ["name", "price"] as const) {}
 
 export class createUserOrderDto {
    @ApiProperty({ description: "구매하려는 상품들" })
--- src/routes/user/dto/product.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PickType } from "@nestjs/swagger";
-import { createProductDto } from "src/routes/admin/dto/create-product.dto";
-
-export class productDto extends PickType(createProductDto, ["name", "price"] as const) {}
