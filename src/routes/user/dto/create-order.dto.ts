1ce529c5cd16c8168a114e8db14d8afb9a4b8725
subin <cdw3521@naver.com>
2024-11-12 13:52:27 +0900

    📝 dto 수정

--- src/routes/user/dto/create-order.dto.ts
+++ src/routes/user/dto/create-order.dto.ts
@@ -1,11 +1,16 @@
 import { ApiProperty, OmitType, PickType } from "@nestjs/swagger";
 import { createUserDto } from "./create-user.dto";
 import { Type } from "class-transformer";
-import { ValidateNested } from "class-validator";
+import { IsNumberString, Length, ValidateNested } from "class-validator";
 import { createProductDto } from "src/routes/admin/dto/create-product.dto";
 
 export class userOrderDto extends OmitType(createUserDto, ["password", "confirmPassword", "agreement"] as const) {}
-export class guestOrderDto extends OmitType(createUserDto, ["agreement"] as const) {}
+export class guestOrderDto extends OmitType(createUserDto, ["password", "agreement"] as const) {
+   @ApiProperty({ description: "비회원 비밀번호" })
+   @IsNumberString()
+   @Length(4, 4, { message: "비밀번호는 4자리 숫자이어야 합니다." })
+   readonly password: string;
+}
 export class productDto extends PickType(createProductDto, ["name", "price"] as const) {}
 
 export class createUserOrderDto {
